b08091a939551ac882232f1ddb40f564059e97f3
Brittany Miltenberger <brittany.miltenberger@gmail.com>
2018-11-03 19:07:45 -0700

    Cleaned up webpack config. Added babel dependencies and babelrc.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,22 @@
-module.exports = {
-  entry: './src/app.js',
+// module.exports = {
+//   entry: './src/app.js',
+//   output: {
+//     filename: './dist/app.bundle.js'
+//   }
+// }
+
+const webpack = require('webpack');
+const path = require('path');
+
+const BUILD_DIR = path.join(__dirname, 'dist');
+const APP_DIR = path.join(__dirname,'src');
+
+const config = {
+  entry: APP_DIR + '/app.js',
   output: {
-    filename: './dist/app.bundle.js'
+    path: BUILD_DIR, 
+    filename: 'app.bundle.js'
   }
 }
+
+module.exports = config;
